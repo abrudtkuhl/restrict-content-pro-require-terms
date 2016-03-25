a8235a2c9519548a410604c676276ed453f0ab8b
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-03-25 12:37:17 -0500

    version bump

--- rcp-require-terms.php
+++ rcp-require-terms.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Restrict Content Pro Terms And Conditions
- * Version: 1.0
+ * Version: 1.1
  * Plugin URI: https://github.com/abrudtkuhl/restrict-content-pro-require-terms
  * Description: Adds an "Accept Terms And Conditions" checkbox to Restrict Content Pro plugin
  * Author: Andy Brudktuhl
